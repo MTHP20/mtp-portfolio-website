5de0ebdd3c42374d4487e7d828ae5d412d9eb986
Michael <mtpanlilio20@hotmail.com>
2024-11-25 23:44:40 +0000

    added info

--- mtp-portfolio/src/sections/home.js
+++ mtp-portfolio/src/sections/home.js
@@ -10,12 +10,13 @@ const Home = () => {
                     <p class="hi">Hi</p>
                     <p class="big-text">I'm Michael</p>
                     <p class="big-text">a Front-End Developer</p>
-                    <p>
-                        As of 2024, I've had a very challenging but rewarding year as over the past 3 years,
-                        I have studied and become a Computer Science Graduate at Royal Holloway, University of London.
-                        In addition to a recent graduate, I'm also newly starting my career as a Junior Front-End Developer
-                        whilst ultimately focusing on expanding my portfolio and greater develop my expertise in various
-                        programming languages with dedication to deliver high-quality solutions.
+                    <br></br>
+                    <p class="info-text">
+                        In 2024, I proudly graduated with a degree in Computer Science from Royal Holloway, University of London,
+                        following three years of rewarding challenges and growth.
+                        <br></br><br></br>
+                        Now embarking on my career as a Junior Front-End Developer, I am committed to expanding my portfolio
+                        and deepening my expertise in programming languages to deliver innovative, high-quality solutions.
                     </p>
                 </div>
                 <div class="picture">
