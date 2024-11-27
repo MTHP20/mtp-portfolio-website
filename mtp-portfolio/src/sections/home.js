aa90d668162b486278dc1e5a80e2d68cf043491b
Michael <mtpanlilio20@hotmail.com>
2024-11-27 19:39:32 +0000

    added slider and photos, home page should be finished for now

--- mtp-portfolio/src/sections/home.js
+++ mtp-portfolio/src/sections/home.js
@@ -1,10 +1,33 @@
 import React from "react";
 import '../App.css';
 import './home.css';
+import './swiper.css';
 import profile from '../images/mainPhoto.jpg';
 import { Link } from 'react-router-dom';
 import { CCol, CRow, CContainer } from '@coreui/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import img1 from '../images/spain/1.JPEG';
+import img2 from '../images/spain/2.JPEG';
+import img3 from '../images/spain/3.JPEG';
+import img4 from '../images/spain/4.JPEG';
+import img5 from '../images/spain/5.JPEG';
+import img6 from '../images/spain/6.JPEG';
+import img7 from '../images/spain/7.JPEG';
+import img8 from '../images/spain/8.JPEG';
+import img9 from '../images/spain/9.JPEG';
+import img10 from '../images/spain/10.JPEG';
+import img11 from '../images/spain/11.JPEG';
+import img12 from '../images/spain/12.JPEG';
 
+const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12];
+const captions = [
+    'Caption 1', 'Umbrella Street in Poble Espanyol', 'TOURISTS GO HOME', 'Basílica de la Sagrada Família',
+    'Caption 5', 'Caption 6', 'Park Güell', 'Caption 8',
+    'Mercat de la Boqueria', 'Fútbol at Park Güell', 'Caption 11', 'Barceloneta Beach'
+];
 
 const Home = () => {
     return (
@@ -32,7 +55,7 @@ const Home = () => {
                     <img src={profile} class="profile" alt="profile" />
                 </div>
             </div>
-            <div class="second">
+            <div class="first">
                 <CContainer class="contain">
                     <CRow class="yesyes" xs={{ cols: 2 }}>
                         <CCol >
@@ -52,9 +75,28 @@ const Home = () => {
                     </CRow>
                 </CContainer>
             </div>
-            {/* <div class="second">
-
-            </div> */}
+            <div class="second">
+                <Swiper
+                    slidesPerView={2.75}
+                    spaceBetween={15}
+                    loop={true}
+                    pagination={true}
+                    navigation={false}
+                    modules={[]}
+                    className="mySwiper"
+                >
+                    {images.map((img, index) => (
+                        <SwiperSlide key={index} className="swiper-slide">
+                            <div className="image-container">
+                                <img src={img} alt={`Slide ${index + 1}`} className="slide-image" />
+                                {captions[index] && !captions[index].startsWith('Caption ') && (
+                                    <div className="captionExtra">{captions[index]}</div>
+                                )}
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
         </div>
     );
 };
