23db952784ba88ec83d841b1adfe2182a073c2cd
Michael <mtpanlilio20@hotmail.com>
2024-12-02 19:06:54 +0000

    update

--- mtp-portfolio/src/sections/projects.js
+++ mtp-portfolio/src/sections/projects.js
@@ -15,19 +15,23 @@ const Projects = () => {
                         <video className="d-block w-100" autoPlay muted loop>
                             <source src={vid1} type="video/mp4" />
                         </video>
-                        <CCarouselCaption className="d-none d-md-block color">
-                            <h5>HTML5 Offline Mapping Application</h5>
-                            <p>Some representative placeholder content for the first slide.</p>
-                        </CCarouselCaption>
+                        <a href="https://github.com/MTHP20/MAPS-APPLICATION" target="_blank" rel="noopener noreferrer">
+                            <CCarouselCaption className="d-none d-md-block color auto-width-caption">
+                                <h5>HTML5 Offline Mapping Application</h5>
+                                <p>Maps Application that leverages HTML5 technologies and <br></br> OpenStreetMap which is capable of working in an offline state</p>
+                            </CCarouselCaption>
+                        </a>
                     </CCarouselItem>
                     <CCarouselItem>
                         <video className="d-block w-100" autoPlay muted loop>
                             <source src={vid2} type="video/mp4" />
                         </video>
-                        <CCarouselCaption className="d-none d-md-block color">
-                            <h5>C# Chat Application</h5>
-                            <p>Some representative placeholder content for the first slide.</p>
-                        </CCarouselCaption>
+                        <a href="https://github.com/MTHP20/ChatApp" target="_blank" rel="noopener noreferrer">
+                            <CCarouselCaption className="d-none d-md-block color auto-width-caption">
+                                <h5>C# Chat Application</h5>
+                                <p>Basic Chat Application that can connect multiple users <br /> to send and receive messages on a server.</p>
+                            </CCarouselCaption>
+                        </a>
                     </CCarouselItem>
                 </CCarousel>
             </div>
